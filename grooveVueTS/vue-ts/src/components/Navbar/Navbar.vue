7c8f385d88ebe3ef87f9c794d5c8300f07d8ef92
vtotskas <vtotskas@advisable.gr>
2021-11-27 19:31:34 +0200

    slider simple frontend vue

--- grooveVueTS/vue-ts/src/components/Navbar/Navbar.vue
+++ grooveVueTS/vue-ts/src/components/Navbar/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
       <router-link to="/" class="navbar-brand"><strong class="p-1">grooveShop</strong></router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- grooveVueTS/vue-ts/src/pages/Home.vue
+++ grooveVueTS/vue-ts/src/pages/Home.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="home">
-    <div class="container mt-3 mb-5">
+    <div class="container-fluid">
+      <div class="row mb-5">
+        <swiper
+            :style="mainSliderStyle"
+            :navigation="true"
+            :mousewheel="false"
+            :keyboard="true"
+            :grabCursor="true"
+            :pagination='{
+              "clickable": true,
+              "dynamicBullets": false
+            }'
+            class="mySwiper"
+        >
+          <swiper-slide v-for="slide in homepageSlider[0].slides">
+            <img v-bind:src="'http://127.0.0.1:8000' + slide.image">
+          </swiper-slide>
+        </swiper>
+      </div>
+    </div>
+    <div class="container mb-5">
       <div class="row">
         <div class="col-12 mb-5">
           <h2 class="is-size-2 has-text-centered">{{ $t('eshop.test3') }}</h2>
@@ -17,11 +37,14 @@
 
 <script lang="ts">
 import store from '@/store'
+import 'swiper/swiper-bundle.css'
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import { Options, Vue } from "vue-class-component"
 import SliderModel from '@/state/slider/SliderModel'
 import ProductModel from "@/state/product/ProductModel"
 import ProductCard from "@/components/Product/ProductCard.vue"
+SwiperCore.use([Navigation,Pagination,Mousewheel,Keyboard])
+import SwiperCore, { Navigation,Pagination,Mousewheel,Keyboard } from 'swiper'
 
 @Options({
   name: "Home",
@@ -29,10 +52,15 @@ import ProductCard from "@/components/Product/ProductCard.vue"
     ProductCard,
     Swiper,
     SwiperSlide
-  },
+  }
 })
 
 export default class Home extends Vue {
+
+  mainSliderStyle = {
+    '--swiper-navigation-color': '#fff',
+    '--swiper-pagination-color': '#fff'
+  }
 
   get LatestProducts(): [ProductModel] {
     return store.getters['product/getLatestProductData']
@@ -51,3 +79,48 @@ export default class Home extends Vue {
 
 }
 </script>
+
+<style lang="scss">
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  width: 100% !important;
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+
+  /* Center slide text vertically */
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  -webkit-align-items: center;
+  align-items: center;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.swiper-pagination-bullet {
+  background-color: black;
+  opacity: 0.35;
+  width: 16px;
+  height: 16px;
+}
+.swiper-pagination-bullet-active {
+  opacity: 1;
+}
+
+</style>
