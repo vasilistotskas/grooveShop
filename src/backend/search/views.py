fc83bd91160f8010bec095511393df58c4ab9a43
vtotskas <vtotskas@advisable.gr>
2022-05-20 11:22:54 +0300

    useless code remove from search/views.py

--- src/backend/search/views.py
+++ src/backend/search/views.py
@@ -1,11 +1,8 @@
 from django.db.models import Q
 from backend.search.paginators import *
-from .filters import SearchProductFilter
 from backend.product.models import Product
 from rest_framework import status, generics
-from rest_framework.response import Response
 from backend.product.serializers import ProductSerializer
-from django_filters.rest_framework import DjangoFilterBackend
 
 
 class SearchProduct(generics.ListAPIView):
@@ -13,11 +10,6 @@ class SearchProduct(generics.ListAPIView):
     pagination_class = SearchPagination
 
     def get_queryset(self):
-        """
-        Optionally restricts the returned purchases to a given user,
-        by filtering against a `username` query parameter in the URL.
-        """
-
         queryset = Product.objects.all()
         query = self.request.query_params.get('query')
 
