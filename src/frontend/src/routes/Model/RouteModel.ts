295ef897e409da04b8125e0783338d6e41534621
vtotskas <vtotskas@advisable.gr>
2022-06-01 21:21:55 +0300

    route model method for beforeEach

--- src/frontend/src/routes/Model/RouteModel.ts
+++ src/frontend/src/routes/Model/RouteModel.ts
@@ -1,5 +1,9 @@
 import { map } from 'lodash'
-import { _RouteRecordBase, RouteRecordNormalized } from 'vue-router'
+import store from '@/store'
+import { useToast } from 'vue-toastification'
+import { _RouteRecordBase, NavigationGuardNext, RouteLocationNormalized, RouteRecordNormalized } from 'vue-router'
+
+const toast = useToast()
 
 export class RouteModel {
 	routeNames: Array<_RouteRecordBase['name']> = []
@@ -16,4 +20,22 @@ export class RouteModel {
 		return new RouteModel(routes)
 	}
 
+	static routeBeforeEach(to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext): void {
+		store.dispatch('auth/initialize').then(() => {
+			if (to.matched.some(record => record.meta.requireLogin) && !store.getters['auth/isAuthenticated']) {
+				toast.error('You are not logged in')
+				next({ name: 'LogIn', query: { to: to.path } })
+			}
+			else {
+				next()
+			}
+		})
+		if (to.name === 'Checkout' && !store.getters['cart/getCartTotalLength']) {
+			toast.error('Your Cart is Empty...')
+			next({ name: 'Cart', query: { to: to.path } })
+		}
+		if (to.name === 'NotFound') {
+			next('/errors/error_404')
+		}
+	}
 }
--- src/frontend/src/routes/index.ts
+++ src/frontend/src/routes/index.ts
@@ -1,4 +1,3 @@
-import store from '@/store'
 import routes from './main.routes'
 import authRoutes from './auth.routes'
 import userRoutes from './user.routes'
@@ -6,13 +5,10 @@ import cartRoutes from './cart.routes'
 import blogRoutes from './blog.routes'
 import searchRoutes from './search.routes'
 import productRoutes from './product.routes'
-import { useToast } from 'vue-toastification'
 import checkoutRoutes from './checkout.routes'
 import categoryRoutes from './category.routes'
 import { RouteModel } from '@/routes/Model/RouteModel'
 import { createRouter, createWebHistory } from 'vue-router'
-
-const toast = useToast()
 
 const router = createRouter({
 	history: createWebHistory(process.env.BASE_URL),
@@ -37,22 +33,7 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-	store.dispatch('auth/initialize').then(() => {
-		if (to.matched.some(record => record.meta.requireLogin) && !store.getters['auth/isAuthenticated']) {
-			toast.error('You are not logged in')
-			next({ name: 'LogIn', query: { to: to.path } })
-		}
-		else {
-			next()
-		}
-	})
-	if (to.name === 'Checkout' && !store.getters['cart/getCartTotalLength']) {
-		toast.error('Your Cart is Empty...')
-		next({ name: 'Cart', query: { to: to.path } })
-	}
-	if (to.name === 'NotFound') {
-		next('/errors/error_404')
-	}
+	RouteModel.routeBeforeEach(to, from, next)
 })
 
 // @TODO experimental
