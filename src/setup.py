39074cd622a3a6ed39cc4f4369e19f45695a4a9c
vtotskas <vtotskas@advisable.gr>
2022-10-02 20:47:32 +0300

    test(workflow): test

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 setup(
     name="grooveShop",
     version=__version__,
-    description="GrooveShop Backend",
+    description="GrooveShop Setup",
     author="Vasilis Totskas",
     author_email="vassilistotskas@msn.com",
 )
