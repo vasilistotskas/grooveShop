a622a4b40535b587edff9d0a9b0049391b9cc8e4
vtotskas <vtotskas@advisable.gr>
2022-12-27 13:00:44 +0200

    chore(Nuxt): added eslint , prettier and some required node packages

--- storefrontUINodeNuxt/src/nuxt.config.ts
+++ storefrontUINodeNuxt/src/nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [
+    // ...
+    '@pinia/nuxt',
+  ],
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        strict: true,
+        types: ['@pinia/nuxt'],
+      },
+    },
+  },
 })
