1e1e55e073f5ceda22e9b08ade8c70a02856f052
vtotskas <vtotskas@advisable.gr>
2023-03-12 23:20:39 +0200

    chore(Nuxt): testing routes

---
+++ storefrontUINodeNuxt/src/pages/testing/bla/bloom/bleh/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+definePageMeta({
+	layout: 'testing',
+	middleware: ['breadcrumbs']
+})
+</script>
+
+<template>
+	<PageWrapper>
+		<PageHeader>
+			<PageTitle :text="$t('pages.testing.index.title')" class="capitalize" />
+		</PageHeader>
+		<PageBody>
+			<PageSection>
+				<p class="text-gray-700 dark:text-gray-200">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia autem debitis ab
+					dolorum tempore placeat possimus perferendis porro sit aut nobis quasi hic
+					consequuntur, atque impedit nihil totam illo odit?
+				</p>
+			</PageSection>
+		</PageBody>
+	</PageWrapper>
+</template>
