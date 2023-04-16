32b5c38622e6d4c948af3b7cd7e6869ff0760bd7
vtotskas <vtotskas@advisable.gr>
2023-04-17 02:25:22 +0300

    chore(Nuxt): translator.js rewrite to .ts

--- storefrontUINodeNuxt/src/tools/translator.ts
+++ storefrontUINodeNuxt/src/tools/translator.ts
@@ -1,13 +1,12 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs')
 const path = require('path')
 const yaml = require('js-yaml')
-const translate = require('translate')
 const args = process.argv.slice(2)
 
 // funcs
-const getArg = (index, def = undefined) => {
+const getArg = (index: number, def: string): string => {
 	try {
 		if (!args[index] && !def) throw new Error('Argument not found')
 		return args[index] || def
@@ -16,9 +15,15 @@ const getArg = (index, def = undefined) => {
 		process.exit(1)
 	}
 }
-const getFiles = (dir) => {
-	const files = []
-	fs.readdirSync(dir).forEach((file) => {
+
+interface LocaleFile {
+	path: string
+	lang: string
+}
+
+const getFiles = (dir: string): LocaleFile[] => {
+	const files: LocaleFile[] = []
+	fs.readdirSync(dir).forEach((file: string) => {
 		const filePath = path.join(dir, file)
 		const stat = fs.statSync(filePath)
 		if (stat.isFile() && file.endsWith('.yml'))
@@ -29,12 +34,20 @@ const getFiles = (dir) => {
 	})
 	return files
 }
-const translateFile = async (file, locale) => {
+
+const translateFile = async (
+	file: Record<string, unknown>,
+	locale: LocaleFile
+): Promise<Record<string, unknown>> => {
 	// file is nested object string
 	// for each all file object child and translate
-	const translatedRes = {}
+	const translatedRes: Record<string, unknown> = {}
+	const translate = (await import('translate')).default // Use dynamic import
 
-	const eachCurrLevel = async (obj, parentKey) => {
+	const eachCurrLevel = async (
+		obj: Record<string, unknown>,
+		parentKey: string
+	): Promise<void> => {
 		for (const key in obj) {
 			const value = obj[key]
 			const nextParent = parentKey ? `${parentKey}.${key}` : key
@@ -43,14 +56,15 @@ const translateFile = async (file, locale) => {
 					to: locale.lang || 'en-US'
 				})
 			} else if (typeof value === 'object') {
-				await eachCurrLevel(value, `${nextParent}`)
+				await eachCurrLevel(value as Record<string, unknown>, `${nextParent}`)
 			}
 		}
 	}
 	await eachCurrLevel(file, '')
 
 	// parse
-	const parseObjLevel = {}
+	const parseObjLevel: Record<string, unknown> = {}
+	let parsedObj
 	for (const key in translatedRes) {
 		let value = translatedRes[key]
 		const keySplit = key.split('.')
@@ -63,7 +77,7 @@ const translateFile = async (file, locale) => {
 			const currKey = keySplit[i]
 			if (!currParent[currKey]) currParent[currKey] = {}
 			if (i === keyLevel - 1) currParent[currKey] = value
-			currParent = currParent[currKey]
+			currParent = currParent[currKey] as Record<string, unknown>
 		}
 
 		parsedObj = parseObjLevel
@@ -80,7 +94,7 @@ const engLocale = path.join(localePath, 'en-US.yml')
 const listLocaleToTranslate = getFiles(localePath).filter((l) => l.lang !== 'en-US')
 
 // main funcs
-async function main() {
+async function main(): Promise<void> {
 	// info
 	console.log('==============================================')
 	console.log(`Target Locales Path: ${localePath}`)
@@ -89,14 +103,19 @@ async function main() {
 
 	// translating
 	console.log('==============================================')
-	console.log(`Starting translate with engine "${translate.engine}"...`)
 	for (const locale of listLocaleToTranslate) {
 		console.log(`Translating ${locale.lang}...`)
 		// remove region code
-		locale.lang = locale.lang.split('-').shift()
+		const localeWithoutRegion = locale.lang.split('-').shift()
+		if (localeWithoutRegion) {
+			locale.lang = localeWithoutRegion
+		}
 		try {
 			// load file
-			const file = yaml.load(fs.readFileSync(engLocale, 'utf8'))
+			const file = yaml.load(fs.readFileSync(engLocale, 'utf8')) as Record<
+				string,
+				unknown
+			>
 
 			// translate
 			const t = await translateFile(file, locale)
@@ -110,3 +129,11 @@ async function main() {
 }
 
 main()
+	.then(() => {
+		console.log('===================== Done =====================')
+		process.exit(0)
+	})
+	.catch((e) => {
+		console.error(e)
+		process.exit(1)
+	})
